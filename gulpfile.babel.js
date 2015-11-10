17fd22c20ac52d33c3a5d138e4d213ba9950678e
Kevin Ivan <kevin.ivan@apsis.com>
2015-11-10 19:12:02 +0100

    gulp stuff

---
+++ gulpfile.babel.js
@@ -0,0 +1,25 @@
+import gulp from 'gulp';
+import stylus from 'gulp-stylus';
+import browserSync from 'browser-sync';
+
+const reload = browserSync.reload;
+
+gulp.task('bs', ['styles'], () => {
+    browserSync({
+        server: {
+            baseDir: './'
+        },
+        browser: ['google chrome canary']
+    });
+
+    gulp.watch(['index.html'], reload);
+    gulp.watch(['css/*.styl'], ['styles', reload]);
+});
+
+gulp.task('styles', () => {
+    return gulp.src('css/styles.styl')
+        .pipe(stylus())
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('default', ['bs']);
